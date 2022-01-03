7d2a545095be3f1f8f61585327f895b6521a9ec7
Ayush Joshi <59890645+AyushJoshi2001@users.noreply.github.com>
2022-01-03 22:02:49 +0530

    setting-up project

--- app.js
+++ app.js
@@ -1,11 +1,30 @@
 const express = require("express");
-const { listen } = require("express/lib/application");
 const app = express();
+const mongoose = require("mongoose");
+
+app.use(express.static("/public"));
 
 app.get("/", (req, res) => {
-  res.send("hello this is poetPad");
+  res.setHeader("Content-Type", "text/plain");
+  res.status(200).send("<h1>hello</h1>");
+});
+
+app.get("/home", (req, res) => {
+  res.status(200).send("success");
+});
+
+app.post("/login", (req, res) => {
+  res.status(200).send("success");
+});
+
+app.post("/signup", (req, res) => {
+  res.status(200).send("success");
+});
+
+app.get("/profile", (req, res) => {
+  res.status(200).send("success");
 });
 
 app.listen(5000, () => {
-  console.log("Server is running on port 5000...");
+  console.log("Server is running on port:5000...");
 });
